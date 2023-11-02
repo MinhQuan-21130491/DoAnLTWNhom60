b699be0a4303e47edee9338aef91431b2e141659
MinhQuan-21130491 <21130491@st.hcmuaf.edu.vn>
2023-11-02 22:57:16 +0700

    update

--- src/main/webapp/ChangePW.js
+++ src/main/webapp/ChangePW.js
@@ -1,40 +1,14 @@
-// function send(){
-//     var pw= document.getElementById('password');
-//     var password = pw.value;
-//     var newPW = document.getElementById('new_password');
-//     var npw=newPW.value;
-//     var rePassword = document.getElementById('re_password');
-//     var repw=rePassword.value;
-//
-//
-//
-//     if(password === ""){
-//         // alert('vui long nhap mat khau');
-//         err.innerHTML='vui lòng nhập mật khẩu';
-//         return;
-//     }
-//     if(password===npw){
-//         alert('mat khau ms trung vs mat khau hien tai');
-//
-//     }
-//     if(npw!==repw){
-//         alert("mat khau nhap lai khong trung vs mat khau moi")
-//     }
-// }
+
 $(document).ready(function (){
     $('#signup-form').submit(function (event){
         var password =$('#password').val();
         var newpassword =$('#new_password').val();
         var repassword =$('#re_password').val();
-
         var condition=true;
-
         if(password ===""){
             $("#errPW").text(' (*)Vui lòng nhập mật khẩu hiện tại');
             $('#errPW').attr('style','color:red');
-
             condition=false;
-
         }else{
             $("#errPW").text('');
         }
@@ -62,9 +36,7 @@ $(document).ready(function (){
             $("#errReNPW").text('');
         }
         if(newpassword===repassword && newpassword!=="" && newpassword!== password){
-
            alert('doi mat khau thanh cong');
-
         }
         if(condition) {
             $(this).submit();
