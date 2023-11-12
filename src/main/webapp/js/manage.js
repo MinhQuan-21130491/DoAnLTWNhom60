8ef6573cd7e5eefd6d61d12216da26db8a79c592
MinhQuan-21130491 <21130491@st.hcmuaf.edu.vn>
2023-11-12 20:10:06 +0700

    update

--- src/main/webapp/js/manage.js
+++ src/main/webapp/js/manage.js
@@ -4,14 +4,13 @@ $(document).ready(function () {
 
     $('#add-product').submit(function (event) {
         var nameP = $('#nameProduct').val();
-        var price = $('#priceProduct').val();
-        var delivery = $('#delivery').val();
+        var priceImp = $('#priceImpProductAdd').val();
+        var price = $('#priceProductAdd').val();
         var des = $('#des').val();
         var img1 = $('#image1').val();
         var img2 = $('#image2').val();
         var img3 = $('#image3').val();
         var img4 = $('#image4').val();
-
         var length = $('#length').val();
         var width = $('#width').val();
         var height = $('#height').val();
@@ -19,87 +18,68 @@ $(document).ready(function () {
         var color = $('#color').val();
         var amount = $('#amount').val();
         var condition = true;
+        var priceReg = /^\d+$/;
         //tên sản phẩm
         if (nameP === "") {
-
             $("#errNameP").text(' (*)Vui lòng nhập tên sản phẩm');
-            $('#errNameP').attr('style', 'font-size:12px');
-            $('#nameProduct').attr('style', 'border-color:red');
             condition = false;
-
         } else {
             $("#errNameP").text('');
-            $('#nameProduct').attr('style', 'border-color:#dee2e6');
         }
         //giá nhập
-        if (price === "") {
-            $("#errPrice").text(' (*)Vui lòng nhập giá sản phẩm!');
-            $('#errPrice').attr('style', 'font-size:12px');
-            $('#priceProduct').attr('style', 'border-color:red');
+        if (priceImp === "") {
+            $("#errImpPriceAdd").text(' (*)Vui lòng nhập giá sản phẩm!');
             condition = false;
-        } else {
-            $("#errPrice").text('');
-            $('#priceProduct').attr('style', 'border-color:#dee2e6');
+        } else if(!priceImp.match(priceReg)) {
+            $("#errImpPriceAdd").text(' (*)Giá tiền không hợp lệ!');
+            condition = false;
+        }else {
+            $("#errImpPriceAdd").text('');
         }
         //giá bán
-        if (delivery === "") {
-            $("#errDelivery").text(' (*)Vui lòng nhập giá bán!');
-            $('#errDelivery').attr('style', 'font-size:12px');
-            $('#delivery').attr('style', 'border-color:red');
+        if (price === "") {
+            $("#errPriceAdd").text(' (*)Vui lòng nhập giá bán!');
             condition = false;
-        } else {
-            $("#errDelivery").text('');
-            $('#delivery').attr('style', 'border-color:#dee2e6');
+        } else if(!price.match(priceReg)) {
+            $("#errPriceAdd").text(' (*)Giá tiền không hợp lệ!');
+            condition = false;
+        }else {
+            $("#errPriceAdd").text('');
         }
         //mô tả
         if (des === "") {
             $("#errDes").text(' (*)Vui lòng nhập mô tả sản phẩm!');
-            $('#errDes').attr('style', 'font-size:12px');
-            $('#des').attr('style', 'border-color:red');
             condition = false;
         } else {
             $("#errDes").text('');
-            $('#des').attr('style', 'border-color:#dee2e6');
         }
         //hình 1
         if (img1 === "") {
             $("#errImg1").text(' (*)Vui lòng nhập đường dẫn hình ảnh 1!');
-            $('#errImg1').attr('style', 'font-size:12px');
-            $('#image1').attr('style', 'border-color:red');
             condition = false;
         } else {
             $("#errImg1").text('');
-            $('#image1').attr('style', 'border-color:#dee2e6');
         }
         //hình 2
         if (img2 === "") {
             $("#errImg2").text(' (*)Vui lòng nhập đường dẫn hình ảnh 2!');
-            $('#errImg2').attr('style', 'font-size:12px');
-            $('#image2').attr('style', 'border-color:red');
             condition = false;
         } else {
             $("#errImg2").text('');
-            $('#image2').attr('style', 'border-color:#dee2e6');
         }
         //hình 3
         if (img3 === "") {
             $("#errImg3").text(' (*)Vui lòng nhập đường dẫn hình ảnh 3!');
-            $('#errImg3').attr('style', 'font-size:12px');
-            $('#image3').attr('style', 'border-color:red');
             condition = false;
         } else {
             $("#errImg3").text('');
-            $('#image3').attr('style', 'border-color:#dee2e6');
         }
         //hình 4
         if (img4 === "") {
             $("#errImg4").text(' (*)Vui lòng nhập đường dẫn hình ảnh 4!');
-            $('#errImg4').attr('style', 'font-size:12px');
-            $('#image4').attr('style', 'border-color:red');
             condition = false;
         } else {
             $("#errImg4").text('');
-            $('#image4').attr('style', 'border-color:#dee2e6');
         }
         //length
         if (length === "") {
@@ -157,9 +137,9 @@ $(document).ready(function () {
     $('#editProduct').submit(function (event) {
 
         var nameP = $('#nameProductEdit').val();
+        var priceImp = $('#priceImpProductEdit').val();
         var price = $('#priceProductEdit').val();
         var des = $('#desEdit').val();
-        var delivery = $('#deliveryEdit').val();
 
         var img1 = $('#image1Edit').val();
         var img2 = $('#image2Edit').val();
@@ -173,93 +153,74 @@ $(document).ready(function () {
         var color = $('#colorEdit').val();
         var amount = $('#amountEdit').val();
         var condition = true;
+        var priceReg = /^\d+$/;
         //tên sản phẩm
         if (nameP === "") {
             $("#errNamePEdit").text(' (*)Vui lòng nhập tên sản phẩm');
-            $('#errNamePEdit').attr('style', 'font-size:12px');
-            $('#nameProductEdit').attr('style', 'border-color:red');
             condition = false;
-
         } else {
             $("#errNamePEdit").text('');
-            $('#nameProductEdit').attr('style', 'border-color:#dee2e6');
         }
         //giá nhập
+        if (priceImp === "") {
+            $("#errImpPriceEdit").text(' (*)Vui lòng nhập giá sản phẩm!');
+            condition = false;
+        } else if(!priceImp.match(priceReg)) {
+            $("#errImpPriceEdit").text(' (*)Giá tiền không hợp lệ!');
+            condition = false;
+        } else{
+            $("#errImpPriceEdit").text('');
+        }
+        //gia ban
         if (price === "") {
-            $("#errPriceEdit").text(' (*)Vui lòng nhập giá sản phẩm!');
-            $('#errPriceEdit').attr('style', 'font-size:12px');
-            $('#priceProductEdit').attr('style', 'border-color:red');
+            $("#errPriceEdit").text(' (*)Vui lòng nhập giá bán!');
             condition = false;
-        }
-        else{
+        }else if(!price.match(priceReg)) {
+                $("#errPriceEdit").text(' (*)Giá tiền không hợp lệ!');
+                condition = false;
+        }else{
             $("#errPriceEdit").text('');
-            $('#priceProductEdit').attr('style', 'border-color:#dee2e6');
-        }
-        //van chuyen
-        if (delivery === "") {
-            $("#errDeliveryEdit").text(' (*)Vui lòng nhập giá bán!');
-            $('#errDeliveryEdit').attr('style', 'font-size:12px');
-            $('#deliveryEdit').attr('style', 'border-color:red');
-            condition = false;
-        }
-        else{
-            $("#errDeliveryEdit").text('');
-            $('#deliveryEdit').attr('style', 'border-color:#dee2e6');
         }
         //mô tả
         if (des === "") {
             $("#errDesEdit").text(' (*)Vui lòng nhập mô tả sản phẩm!');
-            $('#errDesEdit').attr('style', 'font-size:12px');
-            $('#desEdit').attr('style', 'border-color:red');
             condition = false;
         }
         else{
             $("#errDesEdit").text('');
-            $('#desEdit').attr('style', 'border-color:#dee2e6');
         }
         //hình 1
         if (img1 === "") {
             $("#errImg1Edit").text(' (*)Vui lòng nhập đường dẫn hình ảnh 1!');
-            $('#errImg1Edit').attr('style', 'font-size:12px');
-            $('#image1Edit').attr('style', 'border-color:red');
             condition = false;
         }
         else{
             $("#errImg1Edit").text('');
-            $('#image1Edit').attr('style', 'border-color:#dee2e6');
+
         }
         //hình 2
         if (img2 === "") {
             $("#errImg2Edit").text(' (*)Vui lòng nhập đường dẫn hình ảnh 2!');
-            $('#errImg2Edit').attr('style', 'font-size:12px');
-            $('#image2Edit').attr('style', 'border-color:red');
             condition = false;
         }
         else{
             $("#errImg2Edit").text('');
-            $('#image2Edit').attr('style', 'border-color:#dee2e6');
         }
         //hình 3
         if (img3 === "") {
             $("#errImg3Edit").text(' (*)Vui lòng nhập đường dẫn hình ảnh 3!');
-            $('#errImg3Edit').attr('style', 'font-size:12px');
-            $('#image3Edit').attr('style', 'border-color:red');
             condition = false;
         }
         else{
             $("#errImg3Edit").text('');
-            $('#image3Edit').attr('style', 'border-color:#dee2e6');
         }
         //hình 4
         if (img4 === "") {
             $("#errImg4Edit").text(' (*)Vui lòng nhập đường dẫn hình ảnh 4!');
-            $('#errImg4Edit').attr('style', 'font-size:12px');
-            $('#image4Edit').attr('style', 'border-color:red');
             condition = false;
         }
         else{
             $("#errImg4Edit").text('');
-            $('#image4Edit').attr('style', 'border-color:#dee2e6');
         }
         //length
         if (length === "" ) {
