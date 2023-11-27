b16334d62a013823493b57711facd26590c462e3
sangcs321 <21130511@st.hcmuaf.edu.vn>
2023-11-27 22:33:19 +0700

    update forgetPassWord function

--- src/main/java/controller/ForgetPassword.java
+++ src/main/java/controller/ForgetPassword.java
@@ -1,6 +1,6 @@
 package controller;
 
-import javax.servlet.http.HttpServlet;
+import javax.servlet.http.*;
 
 import model.Account;
 import model.VerifyAccount;
@@ -12,8 +12,6 @@ import util.NumberRandom;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.security.SecureRandom;
 import java.sql.Date;
@@ -24,7 +22,7 @@ public class ForgetPassword extends HttpServlet {
     final static String upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     final static String number = "0123456789";
     final static String special = "!@#$&$%*+,-./:;()<=>[]^_`{}|~";
-    public static String genarateRandomString() {
+    public static String generateRandomString() {
         SecureRandom random = new SecureRandom();
         StringBuilder randomString= new StringBuilder();
         randomString.append(lowerCase.charAt(random.nextInt(lowerCase.length())));
@@ -50,33 +48,50 @@ public class ForgetPassword extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doPost(req, resp);
+        doPost(req,resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
         resp.setCharacterEncoding("UTF-8");
+//        resp.setContentType("html/text; charset= UTF-8");
         String username = req.getParameter("user_name");
         String email = req.getParameter("email");
         req.setAttribute("user_name", username);
         req.setAttribute("email", email);
         String err = "";
+
         if (!AccountService.getInstance().checkExistUserName(username)) {
             err = "Tên tài khoản không tồn tại!";
-            req.setAttribute("errUser", err);
+            req.setAttribute("errUserName", err);
         } else if (!AccountService.getInstance().checkExistEmail(email)) {
             err = "Email không tồn tại!";
             req.setAttribute("errEmail", err);
         }else{
             Account account = AccountService.getInstance().selectAccountByUserName(username);
-            String newpassword = genarateRandomString();
+            String newpassword = generateRandomString();
             String pwEncrypt = Encrypt.toSHA1(newpassword);
             if(AccountService.updatePassword(pwEncrypt,account.getId())>0){
-                Email.sendEmail(account.getEmail(),"HomeDecor","Thông tin đăng nhập HomeDecor:\n"+"-Tên đăng nhập: "+username+"\n"+"-Mật khẩu mới: "+newpassword);
-            };
+                Email.sendEmail(account.getEmail(),"Mật khẩu mới của bạn","Thông tin đăng nhập HomeDecor:\n"+"-Tên đăng nhập: "+username+"\n"+"-Mật khẩu mới: "+newpassword);
 
+            };
         }
+        try {
+            String url = "";
+            if(err.length() == 0) {
+                url = "ForgetPW.jsp";
+            }else {
+                url = "SignIn.jsp";
+            }
+            req.getRequestDispatcher(url).forward(req,resp);
+        } catch (ServletException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void main(String[] args) {
 
     }
 
